8e62c44ae7571996ba8015898512b04c94b1c6f4
Kristi <104495880+kristiELLL@users.noreply.github.com>
2023-05-01 23:07:20 +0400

    Counting DNA Nucleotides
    ID - DNA

---
+++ Counting_DNA_Nucleotides.py
@@ -0,0 +1,8 @@
+sample = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
+
+a_count = sample.count("A")
+c_count = sample.count("C")
+g_count = sample.count("G")
+t_count = sample.count("T")
+print(a_count, c_count, g_count, t_count, end=" ")
+
